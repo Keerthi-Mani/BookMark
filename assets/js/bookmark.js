67ecae987b8d9186db734712e23e8b5c5ef6f314
Keerthi Mani <carkeane.getz@gmail.com>
2020-01-24 16:10:04 -0500

    bookmarks are set in local storage

--- assets/js/bookmark.js
+++ assets/js/bookmark.js
@@ -5,6 +5,7 @@ document.getElementById("myForm").addEventListener("submit", saveBookMark);
 function saveBookMark(e) {
   // Prevent form from submitting
   e.preventDefault();
+
   //Get input from form values
   var WebSiteName = document.getElementById("websiteName").value;
   var WebSiteUrl = document.getElementById("websiteUrl").value;
@@ -17,11 +18,21 @@ function saveBookMark(e) {
   };
   //   console.log(bookmark);
 
-  //Local Storage Test
-  //   localStorage.setItem("test", "Hello World");
-  //   console.log(localStorage.getItem("test"));
-  //   localStorage.removeItem("test");
-  //   console.log(localStorage.getItem("test"));
+  //   //Local Storage Test
+  //     localStorage.setItem("test", "Hello World");
+  //     console.log(localStorage.getItem("test"));
+  //     localStorage.removeItem("test");
+  //     console.log(localStorage.getItem("test"));
+
+  // Test if bookmarks is null
+  if (localStorage.getItem("bookmarks") === null) {
+    // initial array
+    var bookmarks = [];
+    //Add to array
+    bookmarks.push(bookmark);
+    // Set to localStorage
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  }
 }
 //Regex Expression
 var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
