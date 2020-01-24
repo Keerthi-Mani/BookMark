c5797533252500cef7eefe6471bab686e459ac1c
Keerthi Mani <carkeane.getz@gmail.com>
2020-01-24 15:57:11 -0500

    prevent default

--- assets/js/bookmark.js
+++ assets/js/bookmark.js
@@ -1,22 +1,22 @@
 // Listen for submit form
 document.getElementById("myForm").addEventListener("submit", saveBookMark);
-var WebSiteName;
-var WebSiteUrl;
+
 //Save BookMark
 function saveBookMark(e) {
+  // Prevent form from submitting
   e.preventDefault();
   //Get input from form values
-  WebSiteName = document.getElementById("websiteName").value;
-  WebSiteUrl = document.getElementById("websiteUrl").value;
+  var WebSiteName = document.getElementById("websiteName").value;
+  var WebSiteUrl = document.getElementById("websiteUrl").value;
   //   console.log(WebSiteName);
   //   console.log(WebSiteUrl);
+
+  var bookmark = {
+    name: WebSiteName,
+    url: WebSiteUrl
+  };
+  //   console.log(bookmark);
 }
-
-var bookmark = {
-  name: WebSiteName,
-  url: WebSiteUrl
-};
-
 //Regex Expression
 var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
 var regex = new RegExp(expression);
